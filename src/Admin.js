73f1c6825540e13257e843da1e628a6cbb288ac4
Angad-Godara <angadrrr2002@gmail.com>
2022-04-23 16:45:02 +0530

    admin temp designed

--- src/Admin.js
+++ src/Admin.js
@@ -4,9 +4,14 @@ import Card from './Card'
 
 function Admin({users, data}) {
   return (
-    <div>
-        <button onClick={users}>Admin</button>
+    <div className='admin'>
+        <button className='button__admin' onClick={users}>Admin</button>
         <div>
+          <div className='header'>
+            <h4>S No.</h4>
+            <h4>Mob No.</h4>
+            <h4>Rating</h4>
+          </div>
         {data.map((item,i) => {
             return <Card id={item.id} rating={item.rating} mob_no={item.mob_no} key={i}/>
         })}
--- src/feedback.js
+++ src/feedback.js
@@ -6,26 +6,26 @@ function feedback({onEventChange}) {
   return (
     <div className='feedback'>
       <Link to='/contact'>
-      <button type='submit' className='button' onClick={ () => onEventChange(1)}>
+      <button type='submit' className='buttn' onClick={ () => onEventChange(1)}>
         <img className='img1' src='https://www.surveystance.com/wp-content/uploads/2020/08/1.png' alt='very bad'></img>
         <h2><strong>1</strong></h2>
         </button>
         </Link>
 
         <Link to='/contact'>
-        <button className='button' onClick={() => onEventChange(2)}>
+        <button className='buttn' onClick={() => onEventChange(2)}>
         <img className='img1' src='https://www.surveystance.com/wp-content/uploads/2020/08/2.png' alt='very bad'></img>
         <h2><strong>2</strong></h2>
         </button>
         </Link>
         <Link to='/contact'>
-        <button className='button' onClick={() => onEventChange(3)}>
+        <button className='buttn' onClick={() => onEventChange(3)}>
         <img className='img1' src='https://www.surveystance.com/wp-content/uploads/2020/08/3.png' alt='very bad'></img>
         <h2><strong>3</strong></h2>
         </button>
         </Link>
         <Link to='/contact'>
-        <button className='button' onClick={() => onEventChange(4)}>
+        <button className='buttn' onClick={() => onEventChange(4)}>
         <img className='img1' src='https://www.surveystance.com/wp-content/uploads/2020/08/4.png' alt='very bad'></img>
         <h2><strong>4</strong></h2>
         </button>
